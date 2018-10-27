fac7140d9271373ea34faca4c43577ac75f7eb67
odipojames <odipojames12@gmai.com>
2018-10-27 14:49:09 +0300

    add progress bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
 
 import { AppComponent } from './app.component';
 import { GithubComponent } from './github/github.component';
@@ -16,7 +18,9 @@ import { ReposearchComponent } from './reposearch/reposearch.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpModule.forRoot() 
   ],
   providers: [],
   bootstrap: [AppComponent]
